be6b68145e303f145cada8f525e71602c587deff
Razin Abrahamyan <abrahamyan.razin@gmail.com>
2023-07-24 17:45:17 +0400

    checking prev item before getting value

--- packages/oceanfront/src/components/TableRow.vue
+++ packages/oceanfront/src/components/TableRow.vue
@@ -354,7 +354,7 @@ export default defineComponent({
               (isOnSelfItem.value || !isOnSelfArea.value) &&
               ((props.dragInfo.isLastChild && isOnSelfItem.value) ||
                 props.depth === 0) &&
-              (isOnTop || singleItem)
+              (isOnTop || singleItem) && prevItem.value
             ) {
               nestedDepth = Math.min(nestedDepth, prevItem.value.depth)
               for (let i = nestedDepth; i >= 0; i--) {
