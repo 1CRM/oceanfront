97db799e621e2862ec632dd74024ea093e46bb28
Roman G <gidion.roman@gmail.com>
2024-09-17 15:37:19 +0300

    Email Marketing: Add WebToLead action #720

--- packages/oceanfront-html-editor/src/extensions/tags.ts
+++ packages/oceanfront-html-editor/src/extensions/tags.ts
@@ -118,5 +118,172 @@ const OneCrmCategory = Node.create({
     return ['onecrm-category', mergeAttributes(HTMLAttributes), 0]
   }
 })
+const Select = Node.create({
+  name: 'select',
+  inline: false,
+  group: 'block',
+  content: 'block+',
 
-export { Span, Abbr, OneCrmArticle, OneCrmCategory }
+  addAttributes() {
+    return {
+      id: {
+        default: null
+      },
+      name: {
+        default: null
+      }
+    }
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'select'
+      }
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['select', mergeAttributes(HTMLAttributes), 0]
+  }
+})
+
+const Option = Node.create({
+  name: 'option',
+  inline: false,
+  group: 'block',
+  content: 'inline*',
+
+  addAttributes() {
+    return {
+      value: {
+        default: null
+      },
+      selected: {
+        default: false
+      }
+    }
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'option'
+      }
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['option', mergeAttributes(HTMLAttributes), 0]
+  }
+})
+
+const Checkbox = Node.create({
+  name: 'checkbox',
+  inline: true,
+  group: 'inline',
+  content: 'inline*',
+
+  addAttributes() {
+    return {
+      id: {
+        default: null
+      },
+      name: {
+        default: null
+      }
+    }
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'input[type="checkbox"]'
+      }
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['input', mergeAttributes({ type: 'checkbox' }, HTMLAttributes)]
+  }
+})
+const InputText = Node.create({
+  name: 'inputText',
+  inline: true,
+  group: 'inline',
+  content: 'inline*',
+
+  addAttributes() {
+    return {
+      id: {
+        default: null
+      },
+      name: {
+        default: null
+      },
+      placeholder: {
+        default: ''
+      },
+      value: {
+        default: ''
+      }
+    }
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'input[type="text"]'
+      }
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['input', mergeAttributes({ type: 'text' }, HTMLAttributes)]
+  }
+})
+
+const InputButton = Node.create({
+  name: 'inputButton',
+  inline: true,
+  group: 'inline',
+  content: 'inline*',
+
+  addAttributes() {
+    return {
+      id: {
+        default: null
+      },
+      name: {
+        default: null
+      },
+      value: {
+        default: ''
+      }
+    }
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'input[type="button"]'
+      }
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['input', mergeAttributes({ type: 'button' }, HTMLAttributes)]
+  }
+})
+
+export {
+  Span,
+  Abbr,
+  OneCrmArticle,
+  OneCrmCategory,
+  Select,
+  Option,
+  Checkbox,
+  InputText,
+  InputButton
+}
